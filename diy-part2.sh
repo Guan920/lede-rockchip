cefe3cb146f5ad3fe1c83f9ebef39ec87b0cac44
Guan920 <97109825+Guan920@users.noreply.github.com>
2024-01-21 21:36:14 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+ZZZ="package/lean/default-settings/files/zzz-default-settings"
+sed -i '/CYXluq4wUazHjmCDBDqXF/d' $ZZZ
+sed -i "s/OpenWrt /DHDAXCW compiled in $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ
